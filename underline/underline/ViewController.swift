a0fdf306b0d50e230ead30aa527f48681a88cb2f
ixx <ixxcome123@gamil.com>
2016-01-08 17:46:48 +0800

    初始化

---
+++ underline/underline/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  underline
+//
+//  Created by apple on 16/1/8.
+//  Copyright © 2016年 www.ixx.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
